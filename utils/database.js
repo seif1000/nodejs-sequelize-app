a7e18a00fafcf3bfee4b3fc2b237b369d046cc62
seif1000 <seifwin74@gmail.com>
2019-09-14 00:13:14 +0100

    fifth  commit

--- utils/database.js
+++ utils/database.js
@@ -13,8 +13,6 @@ if (!global.hasOwnProperty('db')) {
     sequelize = new Sequelize(process.env.DATABASE_URL, {
       dialect:  'postgres',
       protocol: 'postgres',
-      port:     match[4],
-      host:     match[3],
       logging:  true //false
     })
 }else{
