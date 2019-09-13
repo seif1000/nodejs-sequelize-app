023f67a314694a789c6ae09c189403a0db26e773
seif1000 <seifwin74@gmail.com>
2019-09-13 23:54:18 +0100

    second commit

--- utils/database.js
+++ utils/database.js
@@ -1,7 +1,24 @@
+
 const Sequelize = require('sequelize');
 
+let sequelize = null;
 
-const sequelize = new Sequelize('seif','root','benmazouzseifeddine1994', {
+
+if (!global.hasOwnProperty('db')) {
+
+  
+
+  if (process.env.DATABASE_URL) {
+   
+    sequelize = new Sequelize(process.env.HEROKU_POSTGRESQL_BRONZE_URL, {
+      dialect:  'postgres',
+      protocol: 'postgres',
+      port:     match[4],
+      host:     match[3],
+      logging:  true //false
+    })
+}else{
+   sequelize = new Sequelize('seif','root','benmazouzseifeddine1994', {
     dialect: 'mysql',
     host: 'localhost',
     sync:{
@@ -9,6 +26,11 @@ const sequelize = new Sequelize('seif','root','benmazouzseifeddine1994', {
     }
     
   })
+}
+}
+
+
+
 
 
   module.exports = sequelize;
